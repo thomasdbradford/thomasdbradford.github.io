933c0eee0e04dca05f0a6af1dabe882d90b112b3
Thomas Bradford <tdb_1998@hotmail.com>
2019-11-03 17:51:09 +1000

    Updates

---
+++ precache-manifest.8ff75ad5be33ca88c43a9cdf6b2fa5f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d96994c0e5a26649e31607f65a34e9",
+    "url": "/thomasdbradford.github.io/index.html"
+  },
+  {
+    "revision": "315a90545d3bcfcc1849",
+    "url": "/thomasdbradford.github.io/static/css/main.c34a8957.chunk.css"
+  },
+  {
+    "revision": "a6b867cdf85c30c8cf6a",
+    "url": "/thomasdbradford.github.io/static/js/2.66faccda.chunk.js"
+  },
+  {
+    "revision": "315a90545d3bcfcc1849",
+    "url": "/thomasdbradford.github.io/static/js/main.61f2c897.chunk.js"
+  },
+  {
+    "revision": "033a343dbbd222e88778",
+    "url": "/thomasdbradford.github.io/static/js/runtime-main.ff09c3bf.js"
+  },
+  {
+    "revision": "ba851e1dff547d9128572cc4297eafab",
+    "url": "/thomasdbradford.github.io/static/media/Comfortaa-Regular.ba851e1d.ttf"
+  },
+  {
+    "revision": "04247528136af2c45154c0064f2b8bbf",
+    "url": "/thomasdbradford.github.io/static/media/astronaut-background.04247528.jpg"
+  },
+  {
+    "revision": "0fb3adbe87f7f8ebc2fa6153cd6b43a9",
+    "url": "/thomasdbradford.github.io/static/media/portrait1.0fb3adbe.png"
+  }
+]);
