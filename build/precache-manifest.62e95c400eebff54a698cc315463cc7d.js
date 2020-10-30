25f97d903378f1dafa735858de70fe98e8664b69
Thomas Bradford <tdb_1998@hotmail.com>
2020-10-30 18:58:36 +1000

    Add Firebase
    Add Firebase

---
+++ build/precache-manifest.62e95c400eebff54a698cc315463cc7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dd8f49d33675dd555f9c9f9370e1c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "392396e0d5171ed9d031",
+    "url": "/static/css/main.940fd5e0.chunk.css"
+  },
+  {
+    "revision": "9b8c7de852a17aab02bb",
+    "url": "/static/js/2.fa9bcafc.chunk.js"
+  },
+  {
+    "revision": "392396e0d5171ed9d031",
+    "url": "/static/js/main.9bb18fd4.chunk.js"
+  },
+  {
+    "revision": "3f2a1670e73a55e1f494",
+    "url": "/static/js/runtime-main.afd99308.js"
+  },
+  {
+    "revision": "04247528136af2c45154c0064f2b8bbf",
+    "url": "/static/media/astronaut-background.04247528.jpg"
+  },
+  {
+    "revision": "0fb3adbe87f7f8ebc2fa6153cd6b43a9",
+    "url": "/static/media/portrait1.0fb3adbe.png"
+  }
+]);
